33b4f369fe116377aa5b80bd566a3d4f6adcdf7c
Seung Hyun Kim <seung.kim0708@gmail.com>
2025-04-05 15:18:15 -0700

    updated navbar

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,32 +1,46 @@
-import Link from "next/link"
-import { Logo } from "./Logo"
-import { Button } from "@/components/ui/button"
+import { navigationMenuTriggerStyle, NavigationMenu, NavigationMenuList, NavigationMenuItem, NavigationMenuLink } from "@/components/ui/navigation-menu";
+import { Logo } from "./Logo";
+import { Button } from "@/components/ui/button";
+
+import Link from "next/link";
 
 const Header = () => {
   return (
     <header className="py-2 flex justify-between items-center">
-        <Logo />
-        <nav>
-            <ul className="flex gap-10">
-                <li>
-                  <Link href='#about'>About</Link>
-                </li>
-                <li>
-                  <Link href='#solutions'>Solutions</Link>
-                </li>
-                <li>
-                  <Link href='#analytics'>Analytics</Link>
-                </li>
-            </ul>
-        </nav>
-        <div className="flex gap-2">
-            <Button variant="outline">
-              <Link href='/login'>Login</Link>
-            </Button>
-            <Button variant="outline">
-              <Link href="/signup">Sign Up</Link>
-            </Button>
-        </div>
+      <Logo />
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <Link href='#about'>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                About
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href='#solutions'>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Solutions
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href='#analytics'>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Analytics
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+      <div className="flex gap-2">
+        <Button variant="outline">
+          <Link href='/login'>Login</Link>
+        </Button>
+        <Button variant="outline">
+          <Link href="/signup">Sign Up</Link>
+        </Button>
+      </div>
     </header>
   )
 }
