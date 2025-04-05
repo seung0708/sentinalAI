fee0355c269e7564c1a967542606e7066dab9a88
Seung Hyun Kim <seung.kim0708@gmail.com>
2025-04-05 16:06:15 -0700

    got a working responsive navbar. still need to edit it to match our figma design

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,46 +1,11 @@
-import { navigationMenuTriggerStyle, NavigationMenu, NavigationMenuList, NavigationMenuItem, NavigationMenuLink } from "@/components/ui/navigation-menu";
-import { Logo } from "./Logo";
-import { Button } from "@/components/ui/button";
 
-import Link from "next/link";
+import { Navbar1 } from "./Navbar"
+
 
 const Header = () => {
   return (
     <header className="py-2 flex justify-between items-center">
-      <Logo />
-      <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <Link href='#about'>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                About
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link href='#solutions'>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Solutions
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link href='#analytics'>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Analytics
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
-      <div className="flex gap-2">
-        <Button variant="outline">
-          <Link href='/login'>Login</Link>
-        </Button>
-        <Button variant="outline">
-          <Link href="/signup">Sign Up</Link>
-        </Button>
-      </div>
+      <Navbar1 />
     </header>
   )
 }
