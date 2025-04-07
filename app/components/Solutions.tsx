036fab996c2d73f3cfdd352bea8ac46b10b3ebb7
Seung Hyun Kim <seung.kim0708@gmail.com>
2025-04-07 16:03:09 -0700

    added solutions section

---
+++ app/components/Solutions.tsx
@@ -0,0 +1,66 @@
+const Solutions = () => {
+    return (
+        <section id="solutions" className="w-full">
+            <div className="my-0 mb-auto lg:px-16">
+                <h2 className="mb-3 text-xl text-center md:mb-4 md:text-4xl lg:mb-10">
+                    Solutions
+                </h2>
+                <div className="grid gap-6 md:grid-cols-2 lg:gap-8">
+                    <div className="flex flex-col overflow-clip rounded-xl md:col-span-2 md:grid md:grid-cols-2 md:gap-6 lg:gap-8">
+                        <div className="flex flex-col text-center justify-center px-6 py-8 md:px-8 md:py-10 lg:px-10 lg:py-12">
+                            <h3 className="mb-3 text-3xl md:mb-4 md:text-2xl lg:mb-6">
+                                Real-Time Fraud Protection
+                            </h3>
+                            <p className="lg:text-lg">
+                                Detect and prevent fraudulent transactions instantly using AI-driven risk analysis, behavioral scoring, and anomaly detection—reducing chargebacks and financial losses.
+                            </p>
+                        </div>
+                        <div className="md:min-h-[24rem] lg:min-h-[28rem] xl:min-h-[32rem]">
+                            <img
+                                src="https://shadcnblocks.com/images/block/placeholder-1.svg"
+                                alt="Feature 1"
+                                className="aspect-[16/9] h-full w-full object-cover object-center"
+                            />
+                        </div>
+                    </div>
+                    <div className="flex flex-col text-center overflow-clip rounded-xl md:col-span-2 md:grid md:grid-cols-2 md:gap-6 lg:gap-8">
+                        <div className="md:min-h-[24rem] lg:min-h-[28rem] xl:min-h-[32rem]">
+                            <img
+                                src="https://shadcnblocks.com/images/block/placeholder-2.svg"
+                                alt="Feature 2"
+                                className="aspect-[16/9] h-full w-full object-cover object-center"
+                            />
+                        </div>
+                        <div className="flex flex-col justify-center px-6 py-8 md:px-8 md:py-10 lg:px-10 lg:py-12">
+                            <h3 className="mb-3 text-3xl md:mb-4 md:text-2xl lg:mb-6">
+                                Automated Incident Reports
+                            </h3>
+                            <p className="lg:text-lg">
+                                Streamline fraud investigations with automated alerts, risk-based decisioning, and customizable workflows to take action on high-risk transactions faster.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="flex flex-col text-center overflow-clip rounded-xl md:col-span-2 md:grid md:grid-cols-2 md:gap-6 lg:gap-8">
+                        <div className="flex flex-col justify-center px-6 py-8 md:px-8 md:py-10 lg:px-10 lg:py-12">
+                            <h3 className="mb-3 text-3xl md:mb-4 md:text-2xl lg:mb-6">
+                                Advanced Analytics and Reporting
+                            </h3>
+                            <p className="lg:text-lg">
+                                Gain deep insights into fraud trends, risk patterns, and financial impact with interactive dashboards, custom reports, and AI-powered predictions.
+                            </p>
+                        </div>
+                        <div className="md:min-h-[24rem] lg:min-h-[28rem] xl:min-h-[32rem]">
+                            <img
+                                src="https://shadcnblocks.com/images/block/placeholder-2.svg"
+                                alt="Feature 2"
+                                className="aspect-[16/9] h-full w-full object-cover object-center"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Solutions
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import About from "./components/About";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Solutions from "./components/Solutions";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Hero />
         <About />
+        <Solutions />
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
       </footer>
