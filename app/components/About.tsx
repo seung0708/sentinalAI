5cfa71d4ae4a0a1fac4c0a0e5fa363658af79222
Seung Hyun Kim <seung.kim0708@gmail.com>
2025-04-07 15:21:41 -0700

    added about us section

---
+++ app/components/About.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image"
+
+const About = () => {
+  return (
+    <section id="about" className="w-full flex justify-around items-center">
+        <div>
+            <Image 
+             src="/fake.png"
+             width={630}
+             height={800}
+             alt="picture of fake"
+            />
+        </div>
+        <div className="text-center">
+            <h2 className="text-4xl mb-8">About Us</h2>
+            <p className="w-[602px]">
+                Our AI-powered fraud detection platform helps fintech companies identify and prevent fraudulent transactions in real time. 
+                <br />
+                Using advanced machine learning, real-time analytics, and behavioral risk scoring, we empower businesses to minimize losses, reduce chargebacks, and enhance transaction security—all while maintaining a seamless user experience.
+            </p>   
+        </div>
+    </section>
+  )
+}
+
+export default About
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import About from "./components/About";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 
@@ -7,6 +8,7 @@ export default function Home() {
       <Header />
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Hero />
+        <About />
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
       </footer>
