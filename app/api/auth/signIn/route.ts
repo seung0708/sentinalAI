53f1178db6764ea8812c2dde1d101dbdc00cfcd5
Magsz <xmagsz95@gmail.com>
2025-04-09 21:56:10 -0500

    Created user sign in route

--- app/api/auth/signIn/route.ts
+++ app/api/auth/signIn/route.ts
@@ -3,5 +3,34 @@ import {Database} from '@/app/api/types/supabase'
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function POST(req: NextRequest){
-    
+
+        try{
+            const body = await req.json();
+
+            const {email, password} = body;
+
+            const {data: authData, error: dbError} = await supabase.auth.signInWithPassword({
+                email: email,
+                password: password
+            });
+
+            if(dbError){
+                return NextResponse.json({dbError: "Incorrect email or password"},{
+                    status: 400,
+                    headers: {'Content-Type':'application/json'}
+                })
+            }
+
+            return NextResponse.json({message: "Signed In Succesfully"},{
+                status: 200,
+                headers: {'Content-type':'application/json'}
+            })
+
+        }catch(error){
+            return NextResponse.json({error: "Server error"},{
+                status:500,
+                headers: {'Content-Type': 'application/json'}
+            })
+        }
+
 }
