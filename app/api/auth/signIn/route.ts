1ea2d6e1650e10b28974512508081973340899da
Magsz <xmagsz95@gmail.com>
2025-04-09 21:32:06 -0500

    Refactored backend to use next JS route handlers

---
+++ app/api/auth/signIn/route.ts
@@ -0,0 +1,7 @@
+import {supabase} from '@/app/api/lib/supabaseClient'
+import {Database} from '@/app/api/types/supabase'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function POST(req: NextRequest){
+    
+}
