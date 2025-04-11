acb515e71781575ba5a466a54f8b6b69d9f9e57c
Seung Hyun Kim <seung.kim0708@gmail.com>
2025-04-11 16:17:51 -0700

    added more responsive layout to the about and solutions

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -12,8 +12,8 @@ const About = () => {
             />
         </div>
         <div className="text-center">
-            <h2 className="md:text-3xl lg:text-4xl mb-8">About Us</h2>
-            <p className="">
+            <h2 className="text-xl sm:text-3xl md:text-4xl mb-4 md:mb-8">About Us</h2>
+            <p className="max-w-3xl text-base md:text-lg md:text-xl">
                 Our AI-powered fraud detection platform helps fintech companies identify and prevent fraudulent transactions in real time. 
                 <br />
                 Using advanced machine learning, real-time analytics, and behavioral risk scoring, we empower businesses to minimize losses, reduce chargebacks, and enhance transaction security—all while maintaining a seamless user experience.
--- app/components/Solutions.tsx
+++ app/components/Solutions.tsx
@@ -1,17 +1,17 @@
 const Solutions = () => {
     return (
-        <section id="solutions" className="w-full">
+        <section id="solutions" className="w-full mt-4 md:mt-8">
             <div className="my-0 mb-auto lg:px-16">
-                <h2 className="mb-3 text-xl text-center md:mb-4 md:text-4xl lg:mb-10">
+                <h2 className="text-2xl text-center md:mb-4 sm:text-3xl md:text-4xl lg:mb-10">
                     Solutions
                 </h2>
                 <div className="grid gap-6 md:grid-cols-2 lg:gap-8">
                     <div className="flex flex-col overflow-clip rounded-xl md:col-span-2 md:grid md:grid-cols-2 md:gap-6 lg:gap-8">
-                        <div className="flex flex-col text-center justify-center px-6 py-8 md:px-8 md:py-10 lg:px-10 lg:py-12">
-                            <h3 className="mb-3 text-3xl md:mb-4 md:text-2xl lg:mb-6">
+                        <div className="flex flex-col text-center justify-center px-6 py-2 md:px-4 md:py-6 lg:px-10 lg:py-12">
+                            <h3 className="mb-3 text-xl md:mb-4 md:text-2xl lg:mb-6">
                                 Real-Time Fraud Protection
                             </h3>
-                            <p className="lg:text-lg">
+                            <p className="text-base md:text-lg">
                                 Detect and prevent fraudulent transactions instantly using AI-driven risk analysis, behavioral scoring, and anomaly detection—reducing chargebacks and financial losses.
                             </p>
                         </div>
