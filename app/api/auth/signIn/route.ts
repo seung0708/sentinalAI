3f54a71014f6311115b06b200768f10a8c57d369
Seung Hyun Kim <seung.kim0708@gmail.com>
2025-04-16 16:34:31 -0700

    refactoring authentication to using nextj server-side auth

--- app/api/auth/signIn/route.ts
+++ app/api/auth/signIn/route.ts
@@ -1,36 +1,34 @@
-import {supabase} from '@/app/api/lib/supabaseClient'
+import {createClient} from '@/utils/supabase/server'
 import {Database} from '@/app/api/types/supabase'
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function POST(req: NextRequest){
+    const supabase = await createClient();
+    try{
+        const body = await req.json();
+        const {email, password} = body;
+ 
+        const {data: authData, error: dbError} = await supabase.auth.signInWithPassword({
+            email: email,
+            password: password
+        });
 
-        try{
-            const body = await req.json();
-
-            const {email, password} = body;
-
-            const {data: authData, error: dbError} = await supabase.auth.signInWithPassword({
-                email: email,
-                password: password
-            });
-
-            if(dbError){
-                return NextResponse.json({dbError: "Incorrect email or password"},{
-                    status: 400,
-                    headers: {'Content-Type':'application/json'}
-                })
-            }
-
-            return NextResponse.json({message: "Signed In Succesfully"},{
-                status: 200,
-                headers: {'Content-type':'application/json'}
-            })
-
-        }catch(error){
-            return NextResponse.json({error: "Server error"},{
-                status:500,
-                headers: {'Content-Type': 'application/json'}
+        if(dbError){
+            return NextResponse.json({dbError: "Incorrect email or password"},{
+                status: 400,
+                headers: {'Content-Type':'application/json'}
             })
         }
+
+        return NextResponse.json({message: "Signed In Succesfully"},{
+            status: 200,
+            headers: {'Content-type':'application/json'}
+        })
+
+    }catch(error){
+        return NextResponse.json({error: "Server error"},{
+            status:500
+        })
+    }
 
 }
