da2baabe7dfe228b7f2dd8c80fcdad2e50aa777b
Seung Hyun Kim <seung.kim0708@gmail.com>
2025-04-21 17:36:00 -0700

    change account to settings route. working on settings layout

---
+++ app/components/settings/sidebar-nav.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
+import { cn } from "@/lib/utils"
+import { buttonVariants } from "@/components/ui/button"
+
+
+
+export function SidebarNav() {
+  const pathname = usePathname()
+
+  return (
+    <nav
+      className={cn(
+        "flex space-x-2 lg:flex-col lg:space-x-0 lg:space-y-1",
+        className
+      )}
+      {...props}
+    >
+      <Link 
+      href>
+      </Link>
+      {items.map((item) => (
+        <Link
+          key={item.href}
+          href={item.href}
+          className={cn(
+            buttonVariants({ variant: "ghost" }),
+            pathname === item.href
+              ? "bg-muted hover:bg-muted"
+              : "hover:bg-transparent hover:underline",
+            "justify-start"
+          )}
+        >
+          {item.title}
+        </Link>
+      ))}
+    </nav>
+  )
+}
---
+++ app/settings/page.tsx
@@ -0,0 +1,7 @@
+import ProfileForm from "@/app/components/users/profile-form"
+
+export default function Account() {
+    return (
+        <ProfileForm />
+    )
+}
