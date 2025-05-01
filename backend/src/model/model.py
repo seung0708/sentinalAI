6bc233e4a19ba2b8c87eef40809338407cb1b42e
Magsz <xmagsz95@gmail.com>
2025-04-30 20:57:24 -0500

    data visualization for model benchmarks added

--- backend/src/model/model.py
+++ backend/src/model/model.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import joblib
-import matplotlib.plylot as plt 
-from sklearn.metric import (
+import matplotlib.pyplot as plt
+from sklearn.metrics import (
     confusion_matrix,
     ConfusionMatrixDisplay, 
     roc_curve,
+    auc,
     roc_auc_score,
     precision_recall_curve,
     average_precision_score
@@ -91,7 +92,38 @@ y_pred = (y_pred_proba > 0.3).astype(int)
 print(classification_report(y_test, y_pred))
 
 #1.CONFUSION MATRIX
+cm = confusion_matrix(y_test, y_pred)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=['Not Fraud','Fraud'])
+disp.plot(cmap=plt.cm.Blues)
+plt.title("Confusion Matrix")
+plt.show()
 
+# ROC CURVE
+fpr, tpr, _ = roc_curve(y_test, y_pred_proba)
+roc_auc = roc_auc_score(y_test, y_pred_proba)
 
-# joblib.dump(model, 'fraud_model_smote.pkl')
-joblib.dump(final_model, 'xgboost_fraud_model_optimized.pkl')
+plt.figure()
+plt.plot(fpr, tpr, color='darkorange', label=f'ROC Curve (AUC = {roc_auc:.2f})')
+plt.plot([0, 1], [0, 1], color='navy', linestyle='--', label='Random Guess')
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate (Recall)')
+plt.title('ROC Curve')
+plt.legend(loc='lower right')
+plt.grid(True)
+plt.show()
+
+# PRECISION RECALL CURVE
+precision, recall, _ = precision_recall_curve(y_test, y_pred_proba)
+ap_score = average_precision_score(y_test, y_pred_proba)
+
+plt.figure()
+plt.plot(recall, precision, color='green', label=f'PR Curve (AP = {ap_score:.2f})')
+plt.xlabel("Recall")
+plt.ylabel('Precision')
+plt.title('Precision-Recall Curve')
+plt.legend(loc='lower left')
+plt.grid(True)
+plt.show()
+
+# # joblib.dump(model, 'fraud_model_smote.pkl')
+# joblib.dump(final_model, 'xgboost_fraud_model_optimized.pkl')
