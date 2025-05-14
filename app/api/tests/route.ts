e8012486be197d77d316b09727550f39edd3ec47
Seung Hyun Kim <seung.kim0708@gmail.com>
2025-05-14 08:56:10 -0700

    create a test payments route

---
+++ app/api/tests/route.ts
@@ -0,0 +1,7 @@
+import { createClient } from "@/utils/supabase/server";
+import { stripe } from "@/app/api/lib/stripe";
+
+//Create a function that randomly creates multiple charges sequentially. 3 times. 
+const generateTransactions = async () => {
+
+}
