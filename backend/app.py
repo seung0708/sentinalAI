9aa78bd8b99e2ceb1ce577a3449409c688ee2ee3
Seung Hyun Kim <seung.kim0708@gmail.com>
2025-06-26 16:23:56 -0700

    got the model to predict that the first transaction I succesfully sent to the flask api is not fraud

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,30 @@
 from flask import Flask, request
+import pandas as pd
+from feature_engineering import add_combined_frequency_risk, add_address_risk, add_amount_risk
 import json
+import joblib
 #from feature_engineering import
+
+intervals = ['5min', '10min', '30min', '1h']
+fixed_thresholds = {'5min': 3, '10min': 5, '30min': 8, '1h': 15}
+
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/predict-fraud', methods=['POST'])
 def predict_fraud():
-    data = request.json
-    print(data)
+    transaction = pd.DataFrame([request.json])
+    transaction['timestamp'] = pd.to_datetime(transaction['timestamp'])
+    transaction['amount'] = transaction['amount'].astype(float)
+    transaction = add_combined_frequency_risk(transaction, intervals, fixed_thresholds)
+    transaction = add_address_risk(transaction)
+    transaction = add_amount_risk(transaction)
+
+    X = transaction[['tx_count_last_5min', 'avg_tx_last_5min', 'risk_fixed_5min', 'risk_combined_5min', 'tx_count_last_10min', 'avg_tx_last_10min', 'risk_fixed_10min', 'risk_combined_10min', 'tx_count_last_30min', 'avg_tx_last_30min', 'risk_fixed_30min', 'risk_combined_30min', 'tx_count_last_1h', 'avg_tx_last_1h', 'risk_fixed_1h', 'risk_combined_1h', 'combined_frequency_risk', 'is_fake_street', 'is_fake_city', 'fake_address_score', 'addr_change_score', 'combined_address_risk', 'avg_amount', 'amount_risk_score']]
+
+    model = joblib.load('model/fraud.pkl')
+    prediction = model.predict(X)
+    print(prediction)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
