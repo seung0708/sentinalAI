b144039bb7ce0cb87faf823ede396a4938a3c5b8
Seung Hyun Kim <seung.kim0708@gmail.com>
2025-07-16 08:00:13 -0700

    refactored app.py file

--- backend/app.py
+++ backend/app.py
@@ -2,29 +2,14 @@
 import pandas as pd
 import joblib
 import shap
-import os 
-
 from feature_engineering import add_combined_frequency_risk, add_address_risk, add_amount_risk
 from flask import Flask, request, jsonify
-from llama_index.core import Document, Settings, StorageContext, VectorStoreIndex
-from llama_index.vector_stores.supabase import SupabaseVectorStore
 
+from generate_index import embed_index_transactions
+from llama_index.core import Settings
 from llama_index.embeddings.openai import OpenAIEmbedding
-from supabase import create_client, Client
 
 Settings.embed_model = OpenAIEmbedding()
-
-vector_store = SupabaseVectorStore(
-    postgres_connection_string = f'postgresql://{os.environ['CONNECTION_STRING_USER']}:{os.environ['CONNECTION_STRING_PASSWORD']}@{os.environ['CONNECTION_STRING_HOST']}.supabase.co:{os.environ['CONNECTION_STRING_PORT']}/postgres', 
-    collection_name='transactions_embeddings'
-)
-
-openai_api_key = os.getenv('OPENAI_API_KEY')
-
-url = os.environ['NEXT_PUBLIC_SUPABASE_URL']
-key = os.environ['NEXT_PUBLIC_SUPABASE_ANON_KEY']
-supabase = create_client(url, key)
-
 
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
@@ -120,52 +105,7 @@ def predict_fraud():
 @app.route('/index-transactions', methods=['post'])
 def index_transactions():
     transactions = request.json
-    documents = []
-    for transaction in transactions: 
-        text = f'Transaction {transaction['stripe_id']} for {transaction['billing_name']} has a {transaction['predicted_risk']} risk of being fraud because '
-        if transaction['predicted_risk'] == 'high' or transaction['predicted_risk']  == 'medium':
-            if transaction['combined_frequency_risk'] >= 0.5: 
-                if transaction['tx_count_last_5min'] >= 3:
-                    text += f'customer made {transaction['tx_count_last_5min']} purchases within 5 minutes. '
-                elif transaction['tx_count_last_10min'] >= 4:
-                    text += f'customer made {transaction['tx_count_last_10min']} purchases within 10 minutes. '
-                elif transaction['tx_count_last_30min'] >= 5: 
-                    text += f'customer made {transaction['tx_count_last_30min']} purchases within 30 minutes. '
-                elif transaction['tx_count_last_1hr'] >= 7:
-                    text += f'customer made {transaction['tx_count_last_1hr']} purchases within 1 hour. '
-            if transaction['amount_risk_score'] >= 0.5:
-                if transaction['amount'] > transaction['avg_amount']: 
-                    text += f'amount is significantlly higher than normal. '
-                elif transaction['amount']  < transaction['avg_amount']:
-                    text += f'amount is significantaly lower than normal. '
-            if transaction['addr_change_score'] >= 0.5 or transaction['fake_address_score'] >= 0.5:
-                if transaction['is_fake_street'] or transaction['is_fake_city']:
-                    text += f'the street or city appears to be fake or invalid. '
-                else:
-                    text += f'the billing address used for this transaction is different from previous ones. '
-        documents.append( 
-            Document(
-                text=text,
-                metadata={
-                    'file_name': f'transaction_{transaction['transaction_id']}', 
-                    'tx_count_last_5min': transaction['tx_count_last_5min'], 
-                    'tx_count_last_10min': transaction['tx_count_last_10min'], 
-                    'tx_count_last_30min': transaction['tx_count_last_30min'],
-                    'tx_count_last_1hr': transaction['tx_count_last_1hr'], 
-                    'combined_frequency_risk': transaction['combined_frequency_risk'], 
-                    'amount_risk_score': transaction['amount_risk_score'], 
-                    'avg_amount': transaction['avg_amount'], 
-                    'addr_change_score': transaction['addr_change_score'],
-                    'fake_address_score':  transaction['fake_address_score'],
-                    'is_fake_street': transaction['is_fake_street'], 
-                    'is_fake_city': transaction['is_fake_city']
-                }
-            )
-        )
-    
-    storage_context = StorageContext.from_defaults(vector_store=vector_store)
-    VectorStoreIndex.from_documents(documents, storage_context)
-
+    embed_index_transactions(transactions)
     return jsonify({'message': 'Embedded and Indexed to LlamaIndex'}), 200
 
 
