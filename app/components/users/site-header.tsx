d146804ba0846a160890bcbbd799459b8a061b20
Seung Hyun Kim <seung.kim0708@gmail.com>
2025-08-02 09:25:42 -0700

    added optional chaining to pathname in site-header.

--- app/components/users/site-header.tsx
+++ app/components/users/site-header.tsx
@@ -8,14 +8,14 @@ import Link from "next/link";
 
 export function SiteHeader() {
   const pathname = usePathname();
-  const pathnames = pathname.split('/').filter(Boolean);
+  const pathnames = pathname?.split('/').filter(Boolean);
   return (
     <header className="group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 flex justify-between items-center h-12 shrink-0 gap-2 border-b transition-[width,height] ease-linear">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
         <SidebarTrigger size="lg" className="-ml-6" />
         <Breadcrumb>
           <BreadcrumbList className="text-base text-white">
-            {pathnames.map((name, index) => {
+            {pathnames?.map((name, index) => {
               const routeTo = '/' + pathnames.slice(0, index + 1).join('/')
               const isLast = index === pathnames.length - 1
       
