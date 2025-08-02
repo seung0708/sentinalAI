1d4663432f567fdb021070b3ea481036a643760e
Seung Hyun Kim <seung.kim0708@gmail.com>
2025-08-02 13:15:54 -0700

    removed charges destructuring

--- pages/api/webhooks/stripe.ts
+++ pages/api/webhooks/stripe.ts
@@ -58,7 +58,7 @@ export default async function handler (req: NextApiRequest, res: NextApiResponse
                     charges: Stripe.ApiList<Stripe.Charge>;
                 };
                 console.log('paymentIntent', paymentIntent)
-                const {id, amount, created, customer, charges, status, payment_method, payment_method_types} = paymentIntent
+                const {id, amount, created, customer, status, payment_method, payment_method_types} = paymentIntent
                 const paymentIntentCustomerId = customer
                 const {data: accountIdExistsInDb, error: accountError} = await supabase
                     .from('connected_accounts')
